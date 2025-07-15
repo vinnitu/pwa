25c832aa79d40b5168a0dedc031838f511d84f37
Victor Sklyar <vinnitu@gmail.com>
2025-07-15 12:27:17 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'minimal-pwa-cache-v1';
+const OFFLINE_URLS = [
+  '/',
+  '/index.html',
+  '/styles.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
